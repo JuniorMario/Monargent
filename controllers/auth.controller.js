e7901291259967210c5694b6253e7ddd68ed2ab3
LMario <junior@degiant.com.br>
2020-05-07 15:08:53 -0300

    :WIP:

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,50 +1,51 @@
 const authServices = require('../services/auth.service')
-const {ProfileValidator} = require('../validators')
+const { ProfileValidator } = require('../validators')
 
-exports.registerUser = async(req, res, next) => {
+exports.registerUser = async (req, res, next) => {
     const {
         value,
         error,
-      } = ProfileValidator.validate(req.body)
+    } = ProfileValidator.validate(req.body)
 
     if (error) {
-          console.log(error)
-          return res.status('401').json('Um ou mais campos são inválidos')
+        return res.status('401').json('Um ou mais campos são inválidos')
     }
-    const result = await authServices.insertUser(value)
-    return res.json(result)
+    await authServices.insertUser(value)
+    return res.json('Cadastrado com sucesso!')
 }
 
-exports.loginUser = async(req, res, next) => {
-   let counter = 0;
-   const users = await authServices.findUsers()
-   const logged = users.filter(user => {
-       counter += 1
-       if (user.email === req.body.email) {
-           if (user.password === req.body.password) {
-               req.profile = req.body
-               req.profile.id = counter
-               return true
-           }
-       }
+exports.loginUser = async (req, res, next) => {
+    let counter = 0;
+    const users = await authServices.findUsers()
+
+    users.filter(async(user) => {
+        counter += 1
+        if (user.email === req.body.email) {
+            if (user.password === req.body.password) {
+                req.profile = req.body
+                req.profile.id = counter
+                req.session.token = await authServices.getToken(req.profile)
+                req.session.save()
+
+                return res.json(req.session.token)
+            }
+        }
+    return res.json("o e-mail não possui cadastro!")
+
     })
 
-    if (logged){
-        req.session.token = await authServices.getToken(req.profile)
-        req.session.save()
-        return res.json(req.session.token)
-    }
-    return res.json("A senha e o email não coincidem")
+ 
+    return res.json("A senha e o email não coincidem!")
 
-    
+
 
 }
 
-exports.login = async(req, res, next) => {
-    res.render("login", {msg: ""})
+exports.login = async (req, res, next) => {
+    res.render("login", { msg: "" })
 }
 
-exports.register = async(req, res, next) => {
-    res.render("register", {msg: ""})
+exports.register = async (req, res, next) => {
+    res.render("register", { msg: "" })
     return res.json(result)
 }
